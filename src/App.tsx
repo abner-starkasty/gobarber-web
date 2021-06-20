67f53058b84650ad2cc736f67be106f5a7b69355
Abner Willys <abwillys001@gmail.com>
2021-06-20 14:19:30 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyle from './styles/global'
+
 const App = () => {
-  return <h1>Hello Gobarber</h1>
+  return (
+    <>
+      <h1>Hello Gobarber</h1>
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default App
