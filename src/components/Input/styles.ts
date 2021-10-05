bd50743ba16afbef59fbc3f8abffbeffa9badf1f
Abner Willys <abwillys001@gmail.com>
2021-10-05 12:39:48 -0300

    creating errors tooltip

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components'
+import Tooltip from '../Tooltip'
 
 interface ContainerProps {
   $isFocused: boolean
   $isFilled: boolean
+  $hasError: boolean
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,6 +24,11 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.$hasError &&
+    css`
+      border-color: #c53030;
+    `}
+  ${props =>
     props.$isFocused &&
     css`
       border-color: #ff9000;
@@ -32,6 +39,7 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
 
   input {
     flex: 1;
@@ -46,5 +54,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `
