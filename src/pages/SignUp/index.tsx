7d17b24e9f58107832cb40fd54d5a4f77a8ba5f5
Abner Willys <abwillys001@gmail.com>
2021-10-05 03:21:18 -0300

    Validating register

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,7 @@
+import { useCallback } from 'react'
 import { FiLock, FiArrowLeft, FiMail, FiUser } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import Button from '../../components/Button'
 import Input from '../../components/Input'
@@ -9,9 +11,23 @@ import logoImg from '../../assets/logo.svg'
 import { Container, Content, Background } from './styles'
 
 const SignUp = () => {
-  function handleSubmit(data: any): void {
-    console.log(data)
-  }
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      })
+
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
 
   return (
     <Container>
