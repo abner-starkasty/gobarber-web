cb53daa3d4f899a4a3fd4c1c2321bf72789676a0
Abner Willys <abwillys001@gmail.com>
2021-10-05 03:42:14 -0300

    showing errors on input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -50,6 +50,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         onBlur={handleInputBlur}
         {...rest}
       />
+
+      {error}
     </Container>
   )
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
-import { useCallback } from 'react'
+import { useCallback, useRef } from 'react'
 import { FiLock, FiArrowLeft, FiMail, FiUser } from 'react-icons/fi'
+import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 
@@ -9,10 +10,15 @@ import Input from '../../components/Input'
 import logoImg from '../../assets/logo.svg'
 
 import { Container, Content, Background } from './styles'
+import getValidationErrors from '../../utils/getValidationErrors'
 
 const SignUp = () => {
+  const formRef = useRef<FormHandles>(null)
+
   const handleSubmit = useCallback(async (data: any) => {
     try {
+      formRef.current?.setErrors({})
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -24,8 +30,10 @@ const SignUp = () => {
       await schema.validate(data, {
         abortEarly: false,
       })
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      const errors = getValidationErrors(err as any)
+
+      formRef.current?.setErrors(errors)
     }
   }, [])
 
@@ -36,7 +44,7 @@ const SignUp = () => {
       <Content>
         <img src={logoImg} alt="GoBarber Logo" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Register</h1>
 
           <Input name="name" icon={FiUser} placeholder="Name" />
