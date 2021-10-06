188068208c91c26dcfdb8de38fb379aa4b2851b7
Abner Willys <abwillys001@gmail.com>
2021-10-06 17:08:05 -0300

    authenticated routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/require-default-props */
+import { ComponentType } from 'react'
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom'
+
+import { useAuth } from '../hooks/auth'
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean
+  component: ComponentType
+}
+
+const Route = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}: RouteProps) => {
+  const { isSigned } = useAuth()
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) =>
+        isPrivate === isSigned ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location }, // apenas para armazenar o histórico de rotas no navegador
+            }}
+          />
+        )
+      }
+    />
+  )
+}
+
+export default Route
