7cb8386f9721ae4c1146111de88055a33f489f3e
Abner Willys <abwillys001@gmail.com>
2021-10-06 02:29:25 -0300

    adding and removing toasts

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,44 +1,19 @@
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi'
+import { IToastMessage } from '../../hooks/toast'
 
-import { Container, Toast } from './styles'
+import Toast from './Toast'
 
-const ToastContainer = () => {
+import { Container } from './styles'
+
+interface ToastContainerProps {
+  messages: IToastMessage[]
+}
+
+const ToastContainer = ({ messages }: ToastContainerProps) => {
   return (
     <Container>
-      <Toast $hasDescription>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possível fazer login na aplicação</p>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-
-      <Toast $type="success" $hasDescription={false}>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um erro</strong>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
-
-      <Toast $type="error" $hasDescription>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um erro</strong>
-          <p>Não foi possível fazer login na aplicação</p>
-        </div>
-
-        <button type="button">
-          <FiXCircle size={18} />
-        </button>
-      </Toast>
+      {messages.map(message => (
+        <Toast key={message.id} message={message} />
+      ))}
     </Container>
   )
 }
